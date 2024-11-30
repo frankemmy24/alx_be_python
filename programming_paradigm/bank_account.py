94dbefd36f8397dab6e6e8680d6995cab6662cd4
frankemmy24 <emmanuel@opecresearch.com>
2024-11-30 03:34:20 -0800

    commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,22 +1,20 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+        self._account_balance = initial_balance
 
     def deposit(self,amount):
         if amount <= 0:
             print("deposit must a natural number")
         else: 
-            self.account_balance += amount
+            self._account_balance += amount
 
-    def withdrawal(self, amount):
-        if amount <= 0:
-            print("The amount being withdrawn must be a natural number.")
-            return False
-        if amount < self.account_balance:
-            self.account_balance -=amount
+    def withdraw(self, amount):
+        if amount > 0 and self._account_balance > amount:
+            self._account_balance -= amount
             return True
         else:
             print ("Insufficient funds")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance:.2f}")
